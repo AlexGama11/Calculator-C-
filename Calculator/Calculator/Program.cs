656143c6f81163b2884d6525c120c2e9e724dbc9
Alex Gama <52133865+AlexGama11@users.noreply.github.com>
2021-09-27 16:56:25 +0100

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, would you like to use the calculator?");
+            string answer = Console.ReadLine();
+
+            if (answer.Equals("y"))
+            {
+                Console.WriteLine("Choose between +, -, /, *");
+                string option = Console.ReadLine();
+
+                if (option.Equals("+"))
+                {
+                    Console.WriteLine("Input the first number");
+                    int num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Input the second number");
+                    int num2 = int.Parse(Console.ReadLine());
+                    int sum = num1 + num2;
+                    Console.WriteLine("{0} + {1} = {2} ", num1, num2, sum);
+                    Console.ReadKey();
+                }
+
+                if (option.Equals("-"))
+                {
+                    Console.WriteLine("Input the first number");
+                    int num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Input the second number");
+                    int num2 = int.Parse(Console.ReadLine());
+                    int sum = num1 - num2;
+                    Console.WriteLine("{0} - {1} = {2} ", num1, num2, sum);
+                    Console.ReadKey();
+                }
+
+                if (option.Equals("/"))
+                {
+                    Console.WriteLine("Input the first number");
+                    
+                    int num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Input the second number");
+                    int num2 = int.Parse(Console.ReadLine());
+                    int sum = num1 / num2;
+                    Console.WriteLine("{0} / {1} = {2} ", num1, num2, sum);
+                    Console.ReadKey();
+                }
+
+                if (option.Equals("*"))
+                {
+                    Console.WriteLine("Input the first number");
+                    int num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Input the second number");
+                    int num2 = int.Parse(Console.ReadLine());
+                    int sum = num1 * num2;
+                    Console.WriteLine("{0} * {1} = {2} ", num1, num2, sum);
+                    Console.ReadKey();
+                }
+            }
+
+            if (answer.Equals("n"))
+            {
+                return;
+            }
+        }
+    }
+}
